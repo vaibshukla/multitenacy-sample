40a32f3c1a14a1483e661e55ee97507c684ed678
Vaibhav Shukla <cse.vaibhavshukla@gmail.com>
2021-05-13 22:31:15 +0530

    Initial Commit

---
+++ multitenant-service/src/main/java/io/vaibhavshukla/multitenantservice/MultitenantServiceApplication.java
@@ -0,0 +1,13 @@
+package io.vaibhavshukla.multitenantservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultitenantServiceApplication.class, args);
+    }
+
+}
---
+++ multitenant-service/src/test/java/io/vaibhavshukla/multitenantservice/MultitenantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vaibhavshukla.multitenantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
